6c5f10a692782ec6db541a24b466606de4e0b1f1
inzamam.hassan@iqvis.com <inzamam.hassan@iqvis.com>
2018-04-10 02:15:59 +0500

    parsing test now

--- app/Http/Controllers/CoinpaymentsController.php
+++ app/Http/Controllers/CoinpaymentsController.php
@@ -50,7 +50,7 @@ class CoinpaymentsController extends Controller
         $transaction = \Coinpayments::createTransactionSimple($amount, $currency, $currency , array("buyer_email"=> $user->email, "buyer_name"=> $user->name, "item_name"=>"private_sale", "item_number"=>"1", "custom"=> $custom, "ipn_url"=>"https://www.mycapitalco.in/api/ipn"));
         $transaction = json_encode($transaction, true);
         // return ['transaction' => $transaction];
-        return view('dashboard.confirmation', compact('transaction'));
+        return view('dashboard.confirmation')->with('transaction', json_decode($transaction, true));;
     }
 
     /**
