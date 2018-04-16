7928ea6706d4a58eb98590ead300e2cbbaa2e6c3
inzamam.hassan@iqvis.com <inzamam.hassan@iqvis.com>
2018-04-16 12:53:17 +0500

    feedback up[date

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -171,8 +171,8 @@
 								<div class="about-us-content">
 									<p>After years and years of evolution, the Internet has revolutionized the computer and communications world like nothing before, we became capable to interact with anyone despite the long distance between us. It's true that this made our way of life easier, but with what cost?</p>
 								</div>
-								<a target="_blank" href={{ url("technical")}} class="read-more-btn">Read our Technical White Paper</a>
-								<a target="_blank" href={{ url("crowdsale")}} class="read-more-btn">Read our Crowdsale White Paper</a>
+								<a target="_blank" href={{ url("docs/technical")}} class="read-more-btn">Read our Technical White Paper</a>
+								<a target="_blank" href={{ url("docs/crowdsale")}} class="read-more-btn">Read our Crowdsale White Paper</a>
 							</div>
 							<div class="col-md-4 col-lg-4 col-sm-4 text-align-right margin-top-30">
 								<img id="phone-img" class="margin-top-0 iphone-img" src="img/iphone.png" data-aos="zoom-in-right" data-aos-duration="1000">
